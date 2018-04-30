c117a2b9555169a228cf8d53e8d8aa97c248555e
quynhnhule <nhu.le@llwarriors.org>
2018-04-29 20:42:34 -0500

    Display

--- music_player.py
+++ music_player.py
@@ -16,39 +16,42 @@ class Window(QtWidgets.QWidget):
         self.l_playlists = QtWidgets.QLabel("Playlists")
         self.l_current_song = QtWidgets.QLabel("Current song")
         self.songs = QtWidgets.QLabel("Songs: \n1. \n2. \n3. \n4. \n5.")
+        self.line_edit = QtWidgets.QLineEdit()
+        self.line_edit.setText("Search")
+        self.search_button = QtWidgets.QPushButton("search")
 
-        h_box1 = QtWidgets.QHBoxLayout()
+        h_box = QtWidgets.QHBoxLayout()
+        v_box = QtWidgets.QVBoxLayout()
+
+        v_box.addWidget(self.all_song_button)
+        v_box.addWidget(self.l_playlists)
+        v_box.addWidget(self.l_current_song)
+
+        h_box.addLayout(v_box)
+
         v_box1 = QtWidgets.QVBoxLayout()
-
-        v_box1.addWidget(self.all_song_button)
-        v_box1.addWidget(self.l_playlists)
-        v_box1.addWidget(self.l_current_song)
-
-        h_box1.addLayout(v_box1)
+        v_box1.addWidget(self.line_edit)
 
         v_box2 = QtWidgets.QVBoxLayout()
         v_box2.addWidget(self.songs)
 
-        h_box1.addLayout(v_box2)
+        h_box.addLayout(v_box1)
+
+        h_box1 = QtWidgets.QHBoxLayout()
+        h_box1.addWidget(self.shuffle_button)
+        h_box1.addWidget(self.prev_button)
+        h_box1.addWidget(self.play_button)
+        h_box1.addWidget(self.next_button)
 
         h_box2 = QtWidgets.QHBoxLayout()
-        h_box2.addWidget(self.shuffle_button)
+        h_box2.addWidget(self.search_button)
 
-        h_box3 = QtWidgets.QHBoxLayout()
-        h_box3.addWidget(self.prev_button)
+        v_box1.addLayout(h_box2)
+        v_box1.addLayout(v_box2)
+        v_box1.addLayout(h_box1)
 
-        h_box4 = QtWidgets.QHBoxLayout()
-        h_box4.addWidget(self.play_button)
 
-        h_box5 = QtWidgets.QHBoxLayout()
-        h_box5.addWidget(self.next_button)
-
-        v_box2.addLayout(h_box2)
-        v_box2.addLayout(h_box3)
-        v_box2.addLayout(h_box4)
-        v_box2.addLayout(h_box5)
-
-        self.setLayout(h_box1)
+        self.setLayout(h_box)
 
         self.setWindowTitle("Music Player")
         self.setGeometry(100, 100, 800, 600)
