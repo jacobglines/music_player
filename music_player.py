951251d824fa3943edb0565a999697f83caffae1
Jacob Glines <jglines14@yahoo.com>
2018-04-29 22:18:39 -0500

    Adds play function

--- music_player.py
+++ music_player.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
+import PyQt5.QtCore as C
+import PyQt5.QtMultimedia as M
 
 
 class Window(QtWidgets.QWidget):
@@ -15,32 +17,64 @@ class Window(QtWidgets.QWidget):
         self.shuffle_button = QtWidgets.QPushButton("🔀")
         self.l_playlists = QtWidgets.QLabel("Playlists")
         self.l_current_song = QtWidgets.QLabel("Current song")
-        #self.songs = QtWidgets.QLabel("Songs: \n1. \n2. \n3. \n4. \n5.")
-
-        self.l_playlists.setGeometry(50,50,100,10)
+        self.songs = QtWidgets.QLabel("Songs: \n1. \n2. \n3. \n4. \n5.")
+        self.line_edit = QtWidgets.QLineEdit()
+        self.line_edit.setText("Search")
+        self.search_button = QtWidgets.QPushButton("search")
 
         h_box = QtWidgets.QHBoxLayout()
         v_box = QtWidgets.QVBoxLayout()
+
         v_box.addWidget(self.all_song_button)
         v_box.addWidget(self.l_playlists)
         v_box.addWidget(self.l_current_song)
-        #v_box.addWidget(self.songs)
 
         h_box.addLayout(v_box)
 
-        h_box.addWidget(self.shuffle_button)
+        v_box1 = QtWidgets.QVBoxLayout()
+        v_box1.addWidget(self.line_edit)
 
-        h_box.addWidget(self.prev_button)
-        h_box.addWidget(self.play_button)
-        h_box.addWidget(self.next_button)
+        v_box2 = QtWidgets.QVBoxLayout()
+        v_box2.addWidget(self.songs)
+
+        h_box.addLayout(v_box1)
+
+        h_box1 = QtWidgets.QHBoxLayout()
+        h_box1.addWidget(self.shuffle_button)
+        h_box1.addWidget(self.prev_button)
+        h_box1.addWidget(self.play_button)
+        h_box1.addWidget(self.next_button)
+
+        h_box2 = QtWidgets.QHBoxLayout()
+        h_box2.addWidget(self.search_button)
+
+        v_box1.addLayout(h_box2)
+        v_box1.addLayout(v_box2)
+        v_box1.addLayout(h_box1)
+
 
         self.setLayout(h_box)
 
         self.setWindowTitle("Music Player")
         self.setGeometry(100, 100, 800, 600)
         self.show()
+        self.play_button.clicked.connect(self.play)
+
+    def play(self):
+        #
+        # Currently doesn't work
+        #
+        url = C.QUrl.fromLocalFile("/Users/jacobglines/Desktop/Programming/music_player/songs/Maroon 5 - She Will Be Loved.mp3")
+        content = M.QMediaContent(url)
+        player = M.QMediaPlayer()
+        player.setVolume(75)
+        player.setMedia(content)
+        player.play()
+        #
+        # Only works for .wav files
+        #
+        M.QSound.play("/Users/jacobglines/Desktop/dog_growl3.wav")
 
 app = QtWidgets.QApplication(sys.argv)
 window = Window()
 sys.exit(app.exec_())
-
