be4b885135519566d4a49fac7120a9eca15a42cd
quynhnhule <36077663+quynhnhule@users.noreply.github.com>
2018-05-04 08:27:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
+from music_player_1 import MenuBar
 
-from PyQt5 import QtWidgets
-import sys
-from view_interface import MyApplication
-
-def main():
-    #
-    # Setup the application and pass it any command line
-    # arguments that might be present.
-    #
-    pyQtApp = QtWidgets.QApplication(sys.argv)
-    #
-    # Create a new window and show it to the user. Then
-    # start the applications main event loop.
-    #
-    myApplication = MyApplication(pyQtApp)
-    myApplication.show()
-    #
-    # Execute the application (and hence the window), enter the application's
-    # main event loop, and service user requests until the user terminates the
-    # application. Then exit returning and exit conditions.
-    #
-    exitCondition = pyQtApp.exec_()
-    sys.exit(exitCondition)
-
+app = QtWidgets.QApplication(sys.argv)
+window = MenuBar()
+sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
